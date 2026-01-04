68406693d1dc633d2826d7f8af4bf273c12567cf
abab016001 <abab016001@gmail.com>
2026-01-04 13:12:22 +0800

    完成課程6-練習題

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,11 @@ export const onRequest = defineMiddleware(async (context, next) => {
   console.log(`有人訪問了：${context.url.pathname}`);
 
   if (context.url.pathname.startsWith('/admin')) {
-    // 如果沒有登入（這裡模擬邏輯），就跳轉回首頁
-    return context.redirect('/');
+    const pass = context.url.searchParams.get('pass');
+    if (pass != "123") {
+      // 如果沒有登入（這裡模擬邏輯），就跳轉回首頁
+      return context.redirect('/');
+    }
   }
 
   // 繼續執行後續的請求
